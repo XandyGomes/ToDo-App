975105569bca67ac784d19f57e3293af64ab2763
XandyGomes <x3.computadores@hotmail.com>
2020-11-05 15:08:33 -0300

    05.11.2020

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 export default props => (
     <nav className='navbar navbar-inverse bg-inverse'>
         <div className='container'>
-            <a className='navbar-brand' href='#'>
-                <i className='fa fa-calendar-check-o'></i> ToDoApp
-            </a>
-        </div>
+            <div className='navbar-header'>
+                <a className='navbar-brand' href='#'>
+                    <i className='fa fa-calendar-check-o'></i> TodoApp
+                </a>
+            </div>
 
-        <div id='navbar' className='navbar-collapse collapse'>
-            <ul className='nav navbar-nav'>
-                <li><a href='#/todos'>Tarefas</a></li>
-                <li><a href='#/about'>Sobre</a></li>
-            </ul>
+            <div id='navbar' className='navbar-collapse collapse'>
+                <ul className="nav navbar-nav">
+                    <li><a href='#/todos'>Tarefas</a></li>
+                    <li><a href='#/about'>Sobre</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
 )
